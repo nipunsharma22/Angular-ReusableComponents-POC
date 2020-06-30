bd7092d325db274a443ea3b4e2915f36ee4ac2cc
kuldeep.chauhan <kchauhan@rsystems.com>
2020-06-30 17:53:27 +0530

    Grid & Menu bar

--- src/app/gird/gird.component.ts
+++ src/app/gird/gird.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ColumnMode } from '@swimlane/ngx-datatable';
 
 
@@ -8,93 +8,51 @@ import { ColumnMode } from '@swimlane/ngx-datatable';
   styleUrls: ['./gird.component.scss']
 })
 export class GirdComponent implements OnInit {
+  @ViewChild('myTable') table: any;
 
+  rows: any[] = [];
+  expanded: any = {};
+  timeout: any;
 
-  rows = [];
   ColumnMode = ColumnMode;
-  temp: any = [
-  {
-    "name": "Ethel Price",
-    "gender": "female",
-    "company": "Johnson, Johnson and Partners, LLC CMP DDC",
-    "age": 22,
-    "treeStatus": "collapsed"
-  },
-  {
-    "name": "Claudine Neal",
-    "gender": "female",
-    "company": "Sealoud",
-    "age": 55,
-    "treeStatus": "disabled"
-  },
-  {
-    "name": "Beryl Rice",
-    "gender": "female",
-    "company": "Velity",
-    "age": 67,
-    "treeStatus": "disabled"
-  },
-  {
-    "name": "Wilder Gonzales",
-    "gender": "male",
-    "company": "Geekko",
-    "age": 52,
-    "treeStatus": "disabled"
-  },
-  {
-    "name": "Georgina Schultz",
-    "gender": "female",
-    "company": "Suretech",
-    "age": 35,
-    "treeStatus": "collapsed"
-  },
-  {
-    "name": "Carroll Buchanan",
-    "gender": "male",
-    "company": "Ecosys",
-    "age": 42,
-    "treeStatus": "disabled"
-  },
-  {
-    "name": "Valarie Atkinson",
-    "gender": "female",
-    "company": "Hopeli",
-    "age": 27,
-    "treeStatus": "disabled"
-  },
-  {
-    "name": "Schroeder Mathews",
-    "gender": "male",
-    "company": "Polarium",
-    "age": 45,
-    "manager": "Ethel Price",
-    "treeStatus": "disabled"
-  },
-  {
-    "name": "Lynda Mendoza",
-    "gender": "female",
-    "company": "Dogspa",
-    "manager": "Georgina Schultz",
-    "treeStatus": "disabled"
-  }
-];
 
-  constructor() { }
+
+  constructor() { 
+    this.fetch(data => {
+      this.rows = data;
+    });
+  }
 
   ngOnInit(): void {
-    this.rows = this.temp;
+    
   }
 
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/grid-data.json`);
 
-  onTreeAction(event: any) {
-    const index = event.rowIndex;
-    const row = event.row;
-    if (row.treeStatus === 'collapsed') {
-      row.treeStatus = 'expanded';
-    } else {
-      row.treeStatus = 'collapsed';
-    }
-    this.rows = [...this.rows];
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
   }
+
+  toggleExpandRow(row) {
+    console.log('Toggled Expand Row!', row);
+    this.table.rowDetail.toggleExpandRow(row);
+  }
+
+  onDetailToggle(event) {
+    console.log('Detail Toggled', event);
+  }
+
+  onPage(event) {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      console.log('paged!', event);
+    }, 100);
+  }
+
 }
 
