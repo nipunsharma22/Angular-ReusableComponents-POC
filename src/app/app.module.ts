6775ae6ac9a4543c51aad010a7685357b99496f1
Nipun <Nipun.Sharma@rsystems.com>
2020-07-01 15:56:56 +0530

    File uploader updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -67,6 +67,7 @@ import { DatepickerComponent } from './datepicker/datepicker.component';
 import { NotificationComponent } from './notification/notification.component';
 import { DialogComponent } from './dialog/dialog.component';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { FileUploadModule } from 'ng2-file-upload';
 @Injectable()
 export class LowerCaseUrlSerializer extends DefaultUrlSerializer {
   parse(url: string): UrlTree {
@@ -140,7 +141,8 @@ export class LowerCaseUrlSerializer extends DefaultUrlSerializer {
     ChartsModule,
     NgMultiSelectDropDownModule,
     NgxDatatableModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    FileUploadModule
   ],
   providers: [
     Title,
